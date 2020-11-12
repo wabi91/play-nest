e768252cbbfc54150131a9f3c166da39da12cbed
wabi91 <qlqjsk@gmail.com>
2020-11-13 00:35:53 +0900

    feat:  movies route 추가
    - service, controller, entity 추가

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,28 @@
+import { Controller, Get, Param, Delete, Post, Body } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+
+  @Get()
+  getAll() {
+    return this.movieService.getAll();
+  }
+
+  @Post()
+  create(@Body() movieData: any) {
+    return this.movieService.create(movieData);
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') id: string) {
+    return this.movieService.getOne(id);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string) {
+    console.log('id', id);
+    return this.movieService.deleteOne(id);
+  }
+}
