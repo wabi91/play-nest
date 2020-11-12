7ffdf3baf693bad684fb653af97122f1183a5f2e
wabi91 <qlqjsk@gmail.com>
2020-11-13 01:25:18 +0900

    feat: update dto 추가생성
    - nestjs mapped tpyes 모듈 추가

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,6 +9,7 @@ import {
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -35,7 +36,7 @@ export class MoviesController {
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() movieData) {
+  patch(@Param('id') movieId: number, @Body() movieData: UpdateMovieDto) {
     return this.movieService.update(movieId, movieData);
   }
 }
